e3c07df835f6068fedbeb7719d2e4a0ce4500670
dariglad <DariGlad>
2023-01-17 23:43:10 +0500

    add login

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from core.views import RegistrationView
+from core.views import RegistrationView, LoginView
 
 urlpatterns = [
-    path('signup', RegistrationView.as_view(), name='signup')
+    path('signup', RegistrationView.as_view(), name='signup'),
+    path('login', LoginView.as_view(), name='login')
 ]

3e3937ede708792d9ae27dc7a9ad6708ac1cdc3a
dariglad <DariGlad>
2023-01-20 22:48:00 +0500

    add update password user

--- core/views/__init__.py
+++ core/views/__init__.py
@@ -1,9 +1,11 @@
 from .registration import RegistrationView
 from .login import LoginView
 from .profile_logout import ProfileView
+from .update_password import UpdatePasswordView
 
 __all__ = [
     'RegistrationView',
     'LoginView',
-    'ProfileView'
+    'ProfileView',
+    'UpdatePasswordView'
 ]

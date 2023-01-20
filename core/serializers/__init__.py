fcfa2e459b9137797f6de14e74aaf16e2a9f5e5a
dariglad <DariGlad>
2023-01-20 22:48:00 +0500

    add update password user

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,9 +1,11 @@
 from .create_user import CreateUserSerializer
 from .login import LoginSerializer
 from .profile import ProfileSerializer
+from .update_password import UpdatePasswordSerializer
 
 __all__ = [
     'CreateUserSerializer',
     'LoginSerializer',
-    'ProfileSerializer'
+    'ProfileSerializer',
+    'UpdatePasswordSerializer'
 ]

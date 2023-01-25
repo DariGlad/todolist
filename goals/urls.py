30c6a80bd8146b79d2ad72ca9f64abbc6aeadc24
dariglad <DariGlad>
2023-01-25 23:52:11 +0500

    add detail, update, destroy, list for category goal

--- goals/urls.py
+++ goals/urls.py
@@ -5,4 +5,6 @@ from goals import views
 
 urlpatterns = [
     path('goal_category/create', views.GoalCategoryCreateView.as_view(), name='category_create'),
+    path('goal_category/list', views.GoalCategoryCreateView.as_view(), name='category_list'),
+    path('goal_category/<pk>', views.GoalCategoryCreateView.as_view(), name='category_detail'),
 ]
--- goals/views/__init__.py
+++ goals/views/__init__.py
@@ -1,5 +1,7 @@
-from .goal_category import GoalCategoryCreateView
+from .goal_category import GoalCategoryCreateView, GoalCategoryListView, GoalCategoryView
 
 __all__ = [
     'GoalCategoryCreateView',
+    'GoalCategoryListView',
+    'GoalCategoryView',
 ]

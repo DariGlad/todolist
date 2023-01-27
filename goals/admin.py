f7a1a699513904286f9926ae4f6b2b112ea46bad
dariglad <DariGlad>
2023-01-27 15:09:09 +0500

    bug fix

--- goals/admin.py
+++ goals/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from goals.models import (
+    Goal,
+    GoalCategory,
+    GoalComment
+)
+
+
+@admin.register(GoalCategory)
+class GoalCategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'created', 'updated')
+    search_fields = ('title', 'user')
+
+
+admin.site.register(Goal)
+admin.site.register(GoalComment)

7bf69a682c65ee5109d68a7363b251a0ffcf3401
dariglad <DariGlad>
2023-01-29 00:39:55 +0500

    add functional board

--- goals/views/__init__.py
+++ goals/views/__init__.py
@@ -1,6 +1,7 @@
 from .goal_category import GoalCategoryCreateView, GoalCategoryListView, GoalCategoryView
 from .goal import GoalView, GoalListView, GoalCreateView
 from .comment_goal import CommentView, CommentCreateView, CommentListView
+from .board import BoardCreateView, BoardListView, BoardView
 
 __all__ = [
     'GoalCategoryCreateView',
@@ -12,4 +13,7 @@ __all__ = [
     'CommentView',
     'CommentListView',
     'CommentCreateView',
+    'BoardView',
+    'BoardListView',
+    'BoardCreateView',
 ]
